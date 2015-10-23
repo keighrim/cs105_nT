a3eb201f7269823a5f59826052ed07793c73977a
Allan Chesarone <allan.chesarone@mobileheartbeart.com>
2015-10-22 23:51:17 -0400

    Added a follows button, which will follow and unfollow users, as well as update the timeline table to reflect this

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ get '/' do
 		erb :index
 	else
 		@user = logged_in_user
+		@is_current_user = true
 		erb :profile
 	end
 end
@@ -74,26 +75,65 @@ get '/timeline' do
 		#add a view? Or redirect to the log-in page?
 	else
 		tweet_ids = Timeline.where(user_id: u_id).pluck(:tweet_id)
-		@tweets = []
-		tweet_ids.each do |id|
-			@tweets << Tweet.find_by_id(id)
-		end
-
+		@tweets = Tweet.where(id: tweet_ids)
 		erb :timeline
 	end
 end
 
+# post '/follows' do
+# 	u1 = set_user(params[:u1])
+# 	u2 = set_user(params[:u2])
+# 	unless u1.nil? || u2.nil?
+# 		u2.followed_users << u1
+# 		u1.followed_users << u2
+# 		redirect '/'
+# 	else
+# 		"Sorry, there was an error!"
+# 	end
+# end
+
 post '/follows' do
-	u1 = set_user(params[:u1])
-	u2 = set_user(params[:u2])
-	unless u1.nil? || u2.nil?
-		u2.followed_users << u1
-		u1.followed_users << u2
-		redirect '/'
+	user = User.find_by_id(params[:user_id])
+	if user.nil?
+		"Sorry, there was an error"
 	else
-		"Sorry, there was an error!"
+		logged_in_user.followed_users << user
+		redirect back
 	end
 end
+
+post '/unfollows' do
+	user = User.find_by_id(params[:user_id])
+	logged_in_user_id = session[:logged_in_user_id]
+	if user.nil?
+		"Sorry, there was an error"
+	else
+		logged_in_user.followed_users.destroy(user)
+		redirect back
+	end	
+end
+
+get '/users/:user_id' do |user_id|
+	logged_in_user_id = session[:logged_in_user_id]
+	if user_id == logged_in_user_id
+		redirect '/'
+	else
+		@user = User.find_by_id(user_id)
+		if @user.nil?
+			"User does not exist"
+		else
+			@is_current_user = false
+			is_following = logged_in_user.followed_users.include?(@user)
+			if is_following
+				@following = true
+			else
+				@following = false
+			end
+			erb :profile
+		end
+	end
+end
+
 
 def set_user(name)
 	if !name.nil?
--- models/follow.rb
+++ models/follow.rb
@@ -1,4 +1,21 @@
 class Follow < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :followed_user, :class_name => "User"
+
+    after_create :add_followed_tweets
+    before_destroy :remove_followed_tweets
+
+    private
+
+    def add_followed_tweets
+        tweets = User.find_by_id(self.followed_user_id).tweets
+        tweets.each do |tweet|
+            Timeline.create(user_id: self.user_id, tweet_id: tweet.id)
+        end
+    end
+
+    def remove_followed_tweets
+        tweet_ids = User.find_by_id(self.followed_user_id).tweets.pluck(:id)
+        Timeline.where(user_id: self.user_id, tweet_id: tweet_ids).destroy_all
+    end
 end
