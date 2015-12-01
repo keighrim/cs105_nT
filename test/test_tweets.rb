546cfbb2b3eea4cd189c1c26c594378a57226e7d
Allan Chesarone <allan.chesarone@mobileheartbeart.com>
2015-11-30 20:31:21 -0500

    Added the test route /test/tweet/:user_id as per specified in the updated testing guidlines

--- test/test_tweets.rb
+++ test/test_tweets.rb
@@ -15,6 +15,16 @@ module NanoTwitter
           end
           "Created #{num} Tweets"
         end
+
+        app.get '/test/tweet/:user_id' do |user_id|
+          user = User.find_by_id(user_id)
+          if user.nil?
+            "User with id #{user_id} does not exist"
+          else
+            Tweet.make_tweet(user, Faker::Hacker.say_something_smart, Time.now)
+            "User with id #{user_id} has tweeted"
+          end
+        end
       end
     end
   end
