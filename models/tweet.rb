9992d598edddf5646001e43ddb582036921b20ac
SHU CHEN <chenshu8805@gmail.com>
2015-12-08 02:43:00 -0500

    add_to_timeline still bad

--- models/tweet.rb
+++ models/tweet.rb
@@ -10,14 +10,14 @@ class Tweet < ActiveRecord::Base
 
   def add_to_timelines
     #Add to users and followers timelines in redis
-    @user_ids = user.followers.pluck("id")
+    @users_to_insert = user.followers.to_a
     to_add = self.to_json
     $redis.lpushx("timeline:user:#{user.id}",to_add)
     $redis.ltrim("timeline:user:#{user.id}", 0, 49)
-    @user_ids.each do |id|
-      if $redis.exists("timeline:user:#{id}")
-        $redis.lpushx("timeline:user:#{id}", to_add)
-        $redis.ltrim("timeline:user:#{id}", 0, 49)
+    @users_to_insert.each do |u|
+      if $redis.exists("timeline:user:#{u.id}")
+        $redis.lpushx("timeline:user:#{u.id}", to_add)
+        $redis.ltrim("timeline:user:#{u.id}", 0, 49)
       end
     end
   
